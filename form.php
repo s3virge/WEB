8878bf0671328e24a1524c5ee1a0f1b109b34f67
s3virge <kobzarvit@gmail.com>
2017-04-17 20:37:37 +0300

    boilerplate

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Owner
+ * Date: 17.04.2017
+ * Time: 20:12
+ */
+
+// You can access the values posted by jQuery.ajax
+// through the global variable $_POST, like this:
+$bar = isset($_POST['bar']) ? $_POST['bar'] : null;
+
+?>
